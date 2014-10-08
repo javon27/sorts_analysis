bee7e41e375de7d4138e7942de40309adb9e7c42
Jayson Gallardo <jaysontrades@gmail.com>
2014-10-07 22:45:46 -0500

    Added Heap Sort, and fixed updateSize()

--- sorts.py
+++ sorts.py
@@ -102,6 +102,54 @@ def quickSort(arr):
         more = quickSort(more)
         return less + pivotList + more
 
+############################################## HEAP SORT ####################################################
+def heapSort(lst):
+
+    lst = heapify(lst)
+    for start in range((len(lst)-2)//2, -1, -1):
+        siftdown(lst, start, len(lst)-1)
+
+    for end in range(len(lst)-1, 0, -1):
+        lst[end], lst[0] = lst[0], lst[end]
+        siftdown(lst, 0, end - 1)
+    return lst
+
+def siftdown(lst, start, end):
+    global count
+    root = start
+    while True:
+        child = root * 2 + 1
+        if child > end:
+            count += 1
+            break
+        count += 1
+        if child + 1 <= end and lst[child] < lst[child + 1]:
+            child += 1
+        count += 1
+        if lst[root] < lst[child]:
+            lst[root], lst[child] = lst[child], lst[root]
+            root = child
+            count += 1
+        else:
+            count += 1
+            break
+
+def heapify(array):
+    global count
+    size=len(array)
+    for root in range((size//2)-1,-1,-1):
+        root_val = array[root]
+        child = 2*root+1
+        while(child<size):
+            if child<size-1 and array[child]>array[child+1]:
+                child+=1
+            if root_val<=array[child]:     # compare against saved root value
+                break
+            array[(child-1)//2]=array[child]   # find child's parent's index correctly
+            child=2*child+1
+        array[(child-1)//2]=root_val       # here too, and assign saved root value
+    return array
+
 ############################################## HELPER FUNCTIONS ##########################################
 def randomList(size):
     A = [int(size*random()) for i in range(size)]
@@ -154,12 +202,20 @@ def checkArgs(argv):
 
 def updateSize():
     global x
+    global Array
     while True:
         try:
             x = int(input('Array size: '))
             break
         except ValueError:
             print('Not an integer!')
+    if arrayType == 'random':
+        Array = randomList(x)
+    elif arrayType == 'increasing':
+        Array = increasingList(x)
+    else:
+        Array = decreasingList(x)
+
 
 def toggleType():
     global arrayType
@@ -186,11 +242,11 @@ def toggleType():
 def togglePrint():
     global printArray
     printArray = not printArray
-    if printArray:
-        print(Array)
 
 
 def printMenu():
+    if printArray:
+        print('Starting Array: '+str(Array))
     print('\n\n')
     print('Make your selection:')
     print('1: Insertion Sort')
@@ -209,9 +265,6 @@ def main(argv):
 
     Array = randomList(x)
 
-    if printArray:
-        print('Random Array: '+str(A))
-    print('\n')
 
     userInput = None
     while True:
@@ -233,7 +286,7 @@ def main(argv):
             elif userInput == 3:
                 timeIt(quickSort, Array[:])
             elif userInput == 4:
-                #timeIt(heapSort, Array[:])
+                timeIt(heapSort, Array[:])
                 pass
             elif userInput == 5:
                 updateSize()
